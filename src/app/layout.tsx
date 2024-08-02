1f70100eaf7e827adfeda0d2841b60ad88141b4d
Priyobroto Kar <priyobrotokar@gmail.com>
2024-08-03 01:13:32 +0530

    add initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,26 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import localfont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const AeonikFont = localfont({
+  src: [
+    {
+      path: "../../public/fonts/AeonikTRIAL-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/AeonikTRIAL-Light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/AeonikTRIAL-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +34,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${AeonikFont.className} text-base`}>{children}</body>
     </html>
   );
 }
