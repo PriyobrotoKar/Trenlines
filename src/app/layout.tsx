6b7e01fc2ddf0bc69041ac837f9285cb6824b0ac
Priyobroto Kar <priyobrotokar@gmail.com>
2024-09-01 01:35:44 +0530

    feat: Update metadata description in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,38 @@ const redHatDisplay = Red_Hat_Display({
 
 export const metadata: Metadata = {
   title: "Trenlines",
-  description: "",
+  description:
+    "Become financially independent early in the game with Sam Gilkes today",
+  openGraph: {
+    siteName: "Trenlines",
+    title: "Trenlines",
+    description:
+      "Become financially independent early in the game with Sam Gilkes today",
+    type: "website",
+    url: "https://teamtrenlines.com",
+    images: [
+      {
+        url: "https://github.com/PriyobrotoKar/Trenlines/blob/main/public/OG_Thumb.png?raw=true",
+        secureUrl:
+          "https://github.com/PriyobrotoKar/Trenlines/blob/main/public/OG_Thumb.png?raw=true",
+        width: 1200,
+        height: 630,
+        alt: "Trenlines",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@trenlines",
+    creator: "@trenlines",
+    title: "Trenlines",
+    description:
+      "Become financially independent early in the game with Sam Gilkes today",
+    images: {
+      url: "https://github.com/PriyobrotoKar/Trenlines/blob/main/public/OG_Thumb.png?raw=true",
+      alt: "Trenlines",
+    },
+  },
 };
 
 export default function RootLayout({
